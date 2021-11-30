a968cac982dc9cfe2d70c47b706e4924bbb94b7f
Noah <78037393+NoahS219@users.noreply.github.com>
2021-11-30 18:41:21 -0500

    Update main.cpp

--- sortingalgo/main.cpp
+++ sortingalgo/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 #include "search.h"
 
 int main(int argc, char* argv[]) {
-    
+    int textFileType = int argv[1];
+    int sortingAlgoType = int argv[2];
 }

2b2c7e1daefe3c56c8aeb76841ef63efb0c2f4e8
Noah <78037393+NoahS219@users.noreply.github.com>
2021-11-30 18:53:21 -0500

    Update main.cpp

--- sortingalgo/main.cpp
+++ sortingalgo/main.cpp
@@ -3,6 +3,8 @@
 #include "search.h"
 
 int main(int argc, char* argv[]) {
+    std::cout<< "Welcome to the Algorithm Coralle, please pick your File and Algorithm Type using the litters they equate to: \n"
+    std::cout<< "1 = Insertion Sort" << "               " << "2 = Small Sized File (300 Numbers)" << "\n" << "3 = Mergesort" << "               " << "4 = Medium Sized File (700 Numbers)"
     int textFileType = int argv[1];
     int sortingAlgoType = int argv[2];
 }

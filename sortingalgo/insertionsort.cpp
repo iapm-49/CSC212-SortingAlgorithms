efaf1b5fe08b39211ae5167893ef041c32c30577
Isabella Martinez <imartinez4@uri.edu>
2021-11-30 17:30:03 -0500

    Update insertionsort.cpp
    updated insertion sort - still working on it

--- sortingalgo/insertionsort.cpp
+++ sortingalgo/insertionsort.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+
+int insertionSort(int array[], int size){
+    for(int i = 0; i<size; i++){
+        int current = array[i];
+        int j = i-1;
+
+        while(current<array[j] && j>=0){
+            array[j+1] = array[j];
+            --j;
+        }
+        array[j+1] = current;
+    }
+
+}
 
 int main(){
-    
+
+    time_t start, end;
+
+    time(&start);
+
+    insertionSort();
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<microseconds>(stop - start);
+
 }

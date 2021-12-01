6654047b331f7e8f308fbd43f71758bbf288a369
Noah <78037393+NoahS219@users.noreply.github.com>
2021-11-30 19:21:07 -0500

    Update main.cpp

--- sortingalgo/main.cpp
+++ sortingalgo/main.cpp
@@ -6,7 +6,7 @@ int main() {
     int algorithmFileType;
     int textFileType;
     
-    std::cout<< "Welcome to our Algorithm Project, please pick your Sorting Algorithm:";
+    std::cout<< "Welcome to our Algorithm Project, please pick your Sorting Algorithm:" << "\n";
     std::cout<< "1 = Insertion Sort" << "               " << "2 = Merge Sort" << "\n" << "3 = Quick Sort" << "               " << "4 = Count Sort";
     std::cin>> algorithmFileType;
     

23b592dd0cf80f56e07995814d6470cd785ec684
Isabella Martinez <imartinez4@uri.edu>
2021-12-01 21:15:48 -0500

    updated main and insertion sort

--- sortingalgo/countSort.h
+++ sortingalgo/countSort.h
@@ -8,4 +8,4 @@ class CountSort {
         //class call
         
         //
-}
+};
--- sortingalgo/insertionSort.h
+++ sortingalgo/insertionSort.h
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 class InsertionSort {
     public:
         //class constructor
         InsertionSort();
+        InsertionSort(std::string fileType, int size, std::vector<int> &numbers);
+        void writeFile(std::vector<int> &numbers);
+        void doTheSort(std::vector<int> &numbers, int size);
         
         //class call
         
         //
-}
+};

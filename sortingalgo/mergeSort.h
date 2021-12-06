a5be2fc4afeaf88b7030d9e95e16d4b577ea1abc
Noah <78037393+NoahS219@users.noreply.github.com>
2021-12-06 11:33:44 -0500

    Update mergeSort.h

--- sortingalgo/mergeSort.h
+++ sortingalgo/mergeSort.h
@@ -8,6 +8,8 @@ class mergeSort {
         mergeSort();
         
         mergeSort(std::string fileType, int size, std::vector<int> &numbers);
+        void mergeVector(std::vector<int> &numbers, int low, int middle, int high, int size);
+        void mergeSolve(std::vector<int> &numbers, int low, int high, int size);
         void doSort(std::vector<int> &numbers, int size);
         void writeFile(std::vector<int> &numbers);
         

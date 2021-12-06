7b9d90a665fbdd541e178bd45bc69a62f8aba6ff
Isabella Martinez <imartinez4@uri.edu>
2021-12-06 14:00:12 -0500

    Fixed numbers in main

--- sortingalgo/mergeSort.cpp
+++ sortingalgo/mergeSort.cpp
@@ -8,8 +8,7 @@
 
 //This first function reads from the file and assigns it into an vector
 MergeSort::MergeSort(std::string fileType, int size, std::vector<int> &numbers){
-    
-//Reads from the file into an vector and find's it's size
+    //Reads from the file into an vector and find's it's size
     std::ifstream file(fileType);
     if(file){
         int value;
@@ -18,37 +17,6 @@ MergeSort::MergeSort(std::string fileType, int size, std::vector<int> &numbers){
         }
     }
 }
-
-
-void MergeSort::writeFile(std::vector<int> &numbers){
-    // Made a 2d vector so writing to file is easier to read
-    std::vector<std::vector<int>> final;
-	
-    // Row size dependent on size of file
-    int rowSize = numbers.size()/10;
-	
-    // Column size is always 10
-    int colSize = 10;
-	
-    // Nested for loop transforms 1d "numbers" vector into 2d "final" vector
-    for(int i = 0; i < rowSize; i++){
-	    std::vector<int> row;
-	    for(int j = 0; j < colSize; j++){
-		    row.push_back(numbers[(i * 10) + j]);
-	     }
-	    final.push_back(row);
-
-    //create and write to file
-    std::ofstream outFile("sortednumbers.txt");
-    //nested for loop to write final results 
-    for(int i = 0; i < final.size(); i++){
-        for(int j = 0; j < final[0].size(); j++){
-            outFile << final[i][j]<<" ";
-        }
-        outFile << "\n";
-    }
-}
-
 
 void MergeSort::mergeVector(std::vector<int> &numbers, int low, int middle, int high, int size){
     int newLow = middle - low + 1;
@@ -69,7 +37,7 @@ void MergeSort::mergeVector(std::vector<int> &numbers, int low, int middle, int 
     int b = 0;
     int c = low;
 	    
-	while (a <= newLow and b <= newHigh){
+	while (a < newLow and b < newHigh){
 		if (lowVec[a] <= highVec[b]) {
 			numbers[c] = lowVec[a];
 			a++;
@@ -81,7 +49,7 @@ void MergeSort::mergeVector(std::vector<int> &numbers, int low, int middle, int 
 	  c++;
 	}
 
-	while (a <= newLow){
+	while (a < newLow){
 		numbers[c] = lowVec[a];
 		a++;
 		c++;
@@ -111,3 +79,32 @@ void MergeSort::doTheSort(std::vector<int> &numbers, int size){
     mergeSolve(numbers, low, high, size);
 }
     
+void MergeSort::writeFile(std::vector<int> &numbers){
+    // Made a 2d vector so writing to file is easier to read
+    std::vector<std::vector<int> > final;
+	
+    // Row size dependent on size of file
+    int rowSize = numbers.size()/10;
+	
+    // Column size is always 10
+    int colSize = 10;
+	
+    // Nested for loop transforms 1d "numbers" vector into 2d "final" vector
+    for(int i = 0; i < rowSize; i++){
+	    std::vector<int> row;
+	    for(int j = 0; j < colSize; j++){
+		    row.push_back(numbers[(i * 10) + j]);
+	     }
+	    final.push_back(row);
+    }
+
+    //create and write to file
+    std::ofstream outFile("sortednumbers.txt");
+    //nested for loop to write final results 
+    for(int i = 0; i < final.size(); i++){
+        for(int j = 0; j < final[0].size(); j++){
+            outFile << final[i][j]<<" ";
+        }
+        outFile << "\n";
+    }
+}

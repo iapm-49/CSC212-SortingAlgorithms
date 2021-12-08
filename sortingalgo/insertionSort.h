2c6e7bae5fca0210b7eb346bb7a6c4a77f86c095
Isabella Martinez <imartinez4@uri.edu>
2021-12-07 23:23:25 -0500

    updated comments

--- sortingalgo/insertionSort.h
+++ sortingalgo/insertionSort.h
@@ -6,8 +6,11 @@ class InsertionSort {
     public:
         //class constructor
         InsertionSort();
+        //class call
         InsertionSort(std::string fileType, int size, std::vector<int> &numbers);
+        //writes the resulting sorted numbers to a file
         void writeFile(std::vector<int> &numbers);
+        //initiates insertion sort algorithm
         void doTheSort(std::vector<int> &numbers, int size);
         
 };

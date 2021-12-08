0963514df34241779d3f3786c612c8ae9fe88390
Noah <78037393+NoahS219@users.noreply.github.com>
2021-12-07 23:55:59 -0500

    Update mergeSort.h

--- sortingalgo/mergeSort.h
+++ sortingalgo/mergeSort.h
@@ -6,10 +6,15 @@ class MergeSort {
     public:
         //class constructor
         MergeSort();
+        //Merge sorting functions that carry out functions throughout mergeSort
         MergeSort(std::string fileType, int size, std::vector<int> &numbers);
+        //Merge Vector that holds the vectors used within program
         void mergeVector(std::vector<int> &numbers, int low, int middle, int high, int size);
+        //Merge Solve that solves the sorting and placement of lists
         void mergeSolve(std::vector<int> &numbers, int low, int high, int size);
+        // doTheSort that carries out the sorting algorithm
         void doTheSort(std::vector<int> &numbers, int size);
+        // Writes code to file
         void writeFile(std::vector<int> &numbers);
         
 };

27dc4c332506c904af1c2cc4f4580d2c06972cd7
Noah <78037393+NoahS219@users.noreply.github.com>
2021-12-07 23:56:53 -0500

    Update mergeSort.cpp

--- sortingalgo/mergeSort.cpp
+++ sortingalgo/mergeSort.cpp
@@ -18,43 +18,56 @@ MergeSort::MergeSort(std::string fileType, int size, std::vector<int> &numbers){
     }
 }
 
+// Mergesort function that splits the list of elements into two that are ordered and compressed into a large sorted list
 void MergeSort::mergeVector(std::vector<int> &numbers, int low, int middle, int high, int size){
+    
+    // newLow and newHigh variables that are the current set values to test against
     int newLow = middle - low + 1;
     int newHigh = high - middle;
 	
+	// Temporary vectors that hold the upper and lower list to compare
     std::vector<int> lowVec;
     std::vector<int> highVec;
-	    
+	
+	// While i is less than newLow, pushback numbers with low plus i
     for(int i = 0; i < newLow; i++){
 	    lowVec.push_back(numbers[low + i]);
     }
-	
+	// While i is less than newHigh, pushback numbers with middle + j and 1
     for(int j = 0; j < newHigh; j++){
 	    highVec.push_back(numbers[middle + 1 + j]);
     }
     
+    // Variables to be used in the comparison process
     int a = 0;
     int b = 0;
     int c = low;
-	    
+	
+	// While a is less than newLow and b is less than newHigh
 	while (a < newLow and b < newHigh){
+	    // Equal numbers at element c to lowVec at element a, while incrementing a
 		if (lowVec[a] <= highVec[b]) {
 			numbers[c] = lowVec[a];
 			a++;
 		}
+		// if else, set numbers c to highvec at element b and increment b
 		else {
 			numbers[c] = highVec[b];
 			b++;
 		}
 	  c++;
 	}
-
+    
+    //While a is less than newLow
 	while (a < newLow){
+	    //Set numbers at element c to lowVec at element a, increment a and c
 		numbers[c] = lowVec[a];
 		a++;
 		c++;
 	}
+	//While b is less than newHigh
 	while(b < newHigh){
+	    //Set numbers at element c to lowVec at element a, increment b and c
 		numbers[c] = highVec[b];
 		b++;
 		c++;
@@ -73,6 +86,7 @@ void MergeSort::mergeSolve(std::vector<int> &numbers, int low, int high, int siz
     return;
 }
 
+// The Mergesort function that passed in parameters, intiates the merge sort process
 void MergeSort::doTheSort(std::vector<int> &numbers, int size){
     int high = size - 1;
     int low = 0;
